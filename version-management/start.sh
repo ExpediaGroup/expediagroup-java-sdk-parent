f50ce88e824d38280a6ac34a926e1e65258e193d
Automation User <automation@users.noreply.github.com>
2022-09-14 14:54:19 +0300

    build: Automate BOM version management

---
+++ version-management/start.sh
@@ -0,0 +1,28 @@
+function updatePomContent() {
+  git checkout -b "$branchName"
+  version=$(node ./index.js)
+  git add ../bom/pom.xml
+  git commit -m "upgrade bom version to $version"
+  git checkout main
+  git merge "$branchName" || git merge --abort
+}
+
+function retryMerge() {
+  echo "We are facing conflicts and trying to solve it"
+  git checkout "$branchName"
+  git branch -D main
+  git pull
+  git checkout main
+  git branch -D "$branchName"
+  updatePomContent
+}
+git config --global user.name 'github-actions[bot]'
+git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+updatePomContent
+allGood=false
+while true; do
+  git push origin main && allGood=true || retryMerge
+  if $allGood; then
+    break
+  fi
+done
