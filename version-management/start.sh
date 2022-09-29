47669e0ad2aeb7054deafee239a1daba2ef77894
Hamza Nael <hnael99@gmail.com>
2022-09-29 15:45:50 +0300

    chore: create automation user (#10)

--- version-management/start.sh
+++ version-management/start.sh
@@ -16,8 +16,8 @@ function retryMerge() {
   git branch -D "$branchName"
   updatePomContent
 }
-git config --global user.name 'github-actions[bot]'
-git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+git config --global user.name 'eg-oss-ci'
+git config --global user.email 'oss@expediagroup.com'
 updatePomContent
 allGood=false
 while true; do
