5ab7f4ac86c6efd1097beb341c0bee70b4b7917a
mohnoor94 <moh.noor94@gmail.com>
2022-12-01 19:06:07 +0300

    chore(bom): Delete BoM Logic

--- version-management/index.js
+++
@@ -1,5 +0,0 @@
-import PomConverter from './PomConverter.js'
-
-const pomConverter = new PomConverter('../bom/pom.xml')
-const {sdkName, sdkVersion} = process.env
-pomConverter.updateAndSavePomFile(sdkName, sdkVersion);
